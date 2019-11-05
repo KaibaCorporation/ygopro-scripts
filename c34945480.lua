394ee56589c576c71d129969b1a6647f6c26d68a
Kaiba Corporation <kaiba.corporation.github@gmail.com>
2019-11-05 15:03:37 -0500

    Fix Outer Entity Azathot

--- c34945480.lua
+++ c34945480.lua
@@ -30,7 +30,7 @@ function c34945480.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 function c34945480.ovfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0xb6) and c:IsType(TYPE_XYZ)
+	return c:IsFaceup() and c:IsSetCard(0x10b7) and c:IsType(TYPE_XYZ)
 end
 function c34945480.sumcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ)
